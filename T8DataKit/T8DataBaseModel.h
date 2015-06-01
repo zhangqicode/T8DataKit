68efab70363fb17c45c0df95b5a57ba1c93cb516
zhangqi <zhangqi@t8.io>
2015-06-01 13:21:44 +0800

    init commit

---
+++ T8DataKit/T8DataBaseModel.h
@@ -0,0 +1,13 @@
+//
+//  T8DataBaseModel.h
+//  T8DataKitDemo
+//
+//  Created by 琦张 on 15/6/1.
+//  Copyright (c) 2015年 琦张. All rights reserved.
+//
+
+#import "SQLitePersistentObject.h"
+
+@interface T8DataBaseModel : SQLitePersistentObject
+
+@end
---
+++ T8DataKit/T8DataBaseModel.m
@@ -0,0 +1,13 @@
+//
+//  T8DataBaseModel.m
+//  T8DataKitDemo
+//
+//  Created by 琦张 on 15/6/1.
+//  Copyright (c) 2015年 琦张. All rights reserved.
+//
+
+#import "T8DataBaseModel.h"
+
+@implementation T8DataBaseModel
+
+@end
