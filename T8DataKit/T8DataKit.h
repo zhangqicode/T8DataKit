2181a56e4333b6431740c2915ad4b199fe199fb9
zhangqi <zhangqi@t8.io>
2015-10-10 10:10:28 +0800

    add .h file

---
+++ T8DataKit/T8DataKit.h
@@ -0,0 +1,15 @@
+//
+//  T8DataKit.h
+//  T8DataKitDemo
+//
+//  Created by 琦张 on 15/10/10.
+//  Copyright © 2015年 琦张. All rights reserved.
+//
+
+#ifndef T8DataKit_h
+#define T8DataKit_h
+
+#import "T8DataBaseModel.h"
+#import "T8DataBaseManager.h"
+
+#endif /* T8DataKit_h */
